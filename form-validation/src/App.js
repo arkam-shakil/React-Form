f0718c7b42e7acd8daa545707bdb988b0264294f
Arkam <Arkamshakil@gmail.com>
2023-07-11 14:47:04 +0530

    Added list component

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -1,9 +1,12 @@
+import List from './List';
 import Form from './FormComponents/Form';
 import ScreenReaderAnnouncementRegion from './ScreenReaderAnnouncementRegion';
 
 function App() {
 	return (
 		<div className="App">
+			<h3>My List</h3>
+			<List items={["Apple", "Mango", "Banana"]} />
 			<Form />
 			<ScreenReaderAnnouncementRegion />
 		</div>
