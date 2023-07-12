ed54f1521ae49ffd30454f2167928d33015b1707
Arkam <Arkamshakil@gmail.com>
2023-07-12 08:34:13 +0530

    Added a loading indicator

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -1,14 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import List from './ListComponent/List';
 import Form from './FormComponents/Form';
 import ScreenReaderAnnouncementRegion from './ScreenReaderAnnouncementRegionComponent/ScreenReaderAnnouncementRegion';
 
 function App() {
+	const [isLoading, setIsLoading] = useState(true);
+	
+	useEffect(() => {
+		// Simulate a 5-second delay
+		const timer = setTimeout(() => {
+			setIsLoading(false);
+		}, 5000);
+		
+		return () => clearTimeout(timer);
+	}, []);
+	
 	return (
 		<div className="App">
-			<h3>My List</h3>
-			<List items={["Apple", "Mango", "Banana"]} />
-			<Form />
-			<ScreenReaderAnnouncementRegion />
+			{isLoading ? (
+				<div aria-live="assertive">Loading, Please wait...</div> //loading indicator component
+			) : (
+				<>
+				<h3>My List</h3>
+				<List items={["Apple", "Mango", "Banana"]} />
+				<Form />
+				<ScreenReaderAnnouncementRegion />
+				</>
+			)}
 		</div>
 	);
 }
